adab237bb49c673f8c43fdb85bdc20089338eec5
SHENGYANG SHI <shengyangshi@mac.lan>
2025-09-14 16:33:19 -0700

    Working version to load from gpt2 weights and print out value

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,4 +1,5 @@
 import math
+import tiktoken
 import torch
 import torch.nn as nn
 from dataclasses import dataclass
@@ -100,6 +101,24 @@ class GPT(nn.Module):
         )
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
 
+    def forward(self, idx: torch.Tensor):
+        B, T = idx.size()
+        assert (
+            T <= self.config.block_size
+        ), f"Cannot forward sequence of length {T}, block size is {self.config.block_size}"
+
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer["wpe"](pos)
+        tok_emb = self.transformer["wte"](idx)
+        x = tok_emb + pos_emb
+
+        for block in self.transformer["h"]:
+            x = block(x)
+
+        x = self.transformer["ln_f"](x)
+        logits = self.lm_head(x)
+        return logits
+
     @classmethod
     def from_pretrained(cls, model_type: str):
         assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
@@ -152,5 +171,33 @@ class GPT(nn.Module):
         return model
 
 
+device = "mps"
+num_return_sequences = 5
+max_length = 30
+
 model = GPT.from_pretrained("gpt2")
-print("no crash yay")
+model.eval()
+model.to(device)
+
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model")
+tokens = torch.tensor(tokens, dtype=torch.long)  # (8, )
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)  # (5, 8)
+x = tokens.to(device)  # (5, 8)
+
+torch.manual_seed(42)
+
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x)  # (B, T, vocab_size)
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, k=50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)  # (B, 1)
+        xcol = torch.gather(topk_indices, -1, ix)  # (B, 1)
+        x = torch.cat((x, xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
