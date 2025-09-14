e3bb6cdd804a9564ae33f398f63c6f04f861d6fe
SHENGYANG SHI <shengyangshi@mac.lan>
2025-09-14 15:21:18 -0700

    Finish gpt2 implementation

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
+import math
 from torch.nn import functional as F
 
 
@@ -11,6 +12,57 @@ class GPTConfig:
     n_layer: int = 6
     n_head: int = 6
     n_embed: int = 384
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+
+        # key, query, value projection for all heads but in a batch
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, T, C = x.shape
+        qkv = self.c_attn(x)
+
+        q, k, v = qkv.split(self.n_embed, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        att = q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config: GPTConfig) -> None:
+        super().__init__()
+        self.config = config
+        self.layers = nn.Sequential(
+            nn.Linear(config.n_embed, config.n_embed * 4),
+            nn.GELU(approximate="tanh"),
+            nn.Linear(config.n_embed * 4, config.n_embed),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layers(x)
 
 
 class Block(nn.Module):
